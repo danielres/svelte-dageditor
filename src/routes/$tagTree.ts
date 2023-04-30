54e4ba7e82d55e45a88312a7de92a3a0c02bd4fa
Daniel Reszka <dannn.r@gmail.com>
2023-04-30 11:19:45 +0200

    $tagTree: add reset()

--- src/routes/$tagTree.ts
+++ src/routes/$tagTree.ts
@@ -1,12 +1,13 @@
-import { derived, writable } from 'svelte/store'
+import type { Operation } from './types'
 
-export type Operation = { tag: string; from: string | null; to: string | null }
+import { derived, writable } from 'svelte/store'
 
 const store = writable<Operation[]>([])
 
 export const opsStore = {
   ...store,
   add: (operation: Operation) => store.update((ops) => [...ops, operation]),
+  reset: () => store.set([]),
 }
 
 export const opsStoreOptimized = derived(opsStore, ($opsStore) =>
