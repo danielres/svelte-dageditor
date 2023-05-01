5036bde89328f42cc9aa7973a054b4881f92a0a3
Daniel Reszka <dannn.r@gmail.com>
2023-05-01 13:21:10 +0200

    $tagTree: add opsStoreAllowed

--- src/routes/$tagTree.ts
+++ src/routes/$tagTree.ts
@@ -1,3 +1,4 @@
+import { getAncestors, type Tag } from '$lib/utils/tags'
 import type { Operation } from './types'
 
 import { derived, writable } from 'svelte/store'
@@ -10,7 +11,26 @@ export const opsStore = {
   reset: () => store.set([]),
 }
 
-export const opsStoreOptimized = derived(opsStore, ($opsStore) =>
+export const allTags = writable<Tag[]>([])
+
+export const opsStoreAllowed = derived([opsStore, allTags], ([$opsStore, $allTags]) =>
+  $opsStore.reduce((acc, cur) => {
+    // prevent if no destination
+    if (!cur.to) return acc
+
+    // prevent if tag was dropped on itself
+    if (cur.tag === cur.to) return acc
+
+    // prevent if tag was dropped on its ancestor (avoids infinite loops)
+    const ancestors = getAncestors($allTags, cur.to)
+    if (ancestors.some((ancestor) => ancestor.id === cur.tag)) return acc
+
+    // otherwise, allow operation
+    return [...acc, cur]
+  }, [] as Operation[])
+)
+
+export const opsStoreOptimized = derived(opsStoreAllowed, ($opsStore) =>
   $opsStore.reduce((acc, cur) => {
     const found = acc.find((stored) => stored.tag === cur.tag)
     acc = acc.filter((stored) => stored.tag !== cur.tag)
