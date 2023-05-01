2936127f45689f792d1b810c88c25659c77bdc60
Daniel Reszka <dannn.r@gmail.com>
2023-05-01 16:13:41 +0200

    tailwindcss: add

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
+  import '../app.postcss'
 </script>
 
 <nav>
   <ul>
-    <li>
-      <a href="/v1">V1</a>
-    </li>
-    <li>
-      <a href="/v2">V2</a>
-    </li>
+    <li><a href="/v1">V1</a></li>
+    <li><a href="/v2">V2</a></li>
   </ul>
 </nav>
 
