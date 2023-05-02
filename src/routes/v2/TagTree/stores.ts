66dbf96a9fce7a421ee32f5cbca6d33745253e9b
Daniel Reszka <dannn.r@gmail.com>
2023-05-02 23:09:36 +0200

    stores: add isAllowedOperation()

--- src/routes/v2/TagTree/stores.ts
+++ src/routes/v2/TagTree/stores.ts
@@ -3,6 +3,9 @@ import type { Operation, Relation, Tag } from './types'
 import { tick } from 'svelte'
 import { derived, get, writable } from 'svelte/store'
 import { getNestedDescendants } from './stores/getNestedDescendants'
+import { isTruthy, onlyUnique } from '$lib/utils/array'
+
+const MAX_RECURSION_DEPTH = 10
 
 export const relationsStore = writable<Relation[]>([])
 export const tagsStore = writable<Tag[]>([])
@@ -18,6 +21,29 @@ export function makeTagTreeStore(rootId: string) {
 
 function tagNameToTagId(name: string) {
   return get(tagsStore).find((t) => t.name === name)?.id
+}
+
+function getParents(tags: Tag[], tagId: string) {
+  const relations = get(relationsStore).filter((relation) => tagId.includes(relation.childId))
+  const parentIds = relations.map((t) => t.parentId).filter(isTruthy)
+  return tags.filter((tag) => parentIds.includes(tag.id))
+}
+
+function getAncestors(tags: Tag[], tagId: string, depth = 0): Tag[] {
+  const parents = getParents(tags, tagId)
+  if (depth > MAX_RECURSION_DEPTH) return parents
+  const ancestors = parents.flatMap((parent) => getAncestors(tags, parent.id, depth + 1))
+  return [...parents, ...ancestors].filter(onlyUnique)
+}
+
+export function isAllowedOperation({ tag, to }: Operation) {
+  if (!to || !tag) return false
+  if (tag === to) return false
+  const toId = tagNameToTagId(to)
+  if (!toId) return false
+  const ancestors = getAncestors(get(tagsStore), toId)
+  if (ancestors.some((ancestor) => ancestor.name === tag)) return false
+  return true
 }
 
 export async function runOperation() {
