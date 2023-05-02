581559d05955c81d3216692f965079c96c1ad187
Daniel Reszka <dannn.r@gmail.com>
2023-05-02 14:25:38 +0200

    remove default nextOperations

--- src/routes/v2/+page.svelte
+++ src/routes/v2/+page.svelte
@@ -2,14 +2,8 @@
   import type { PageData } from './$types'
 
   import TagTree from './TagTree.svelte'
-  import { nextOperations } from './TagTree/stores'
 
   export let data: PageData
-
-  $nextOperations = [
-    { tag: 't1', from: '<root>', to: 't2' },
-    { tag: 't11', from: 't1', to: '<root>' },
-  ]
 </script>
 
 <TagTree tags={data.tags} relations={data.relations} rootId="<root>" />
