05ba75257ce74082354c0b33a6f31d2a307e93eb
Daniel Reszka <dannn.r@gmail.com>
2023-05-07 15:40:56 +0200

    Icon: add cancel + submit

--- src/routes/v3/[ws]/Icon.svelte
+++ src/routes/v3/[ws]/Icon.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  export let kind: 'add' | 'copy' | 'delete' | 'rename' | 'go' = 'go'
+  export let kind: 'add' | 'cancel' | 'copy' | 'delete' | 'rename' | 'submit' | 'go'
 </script>
 
 <svg
@@ -16,6 +16,9 @@
       stroke-linejoin="round"
       d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
     />
+  {/if}
+  {#if kind === 'cancel'}
+    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
   {/if}
   {#if kind === 'copy'}
     <path
@@ -37,6 +40,9 @@
       stroke-linejoin="round"
       d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
     />
+  {/if}
+  {#if kind === 'submit'}
+    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
   {/if}
   {#if kind === 'go'}
     <path
