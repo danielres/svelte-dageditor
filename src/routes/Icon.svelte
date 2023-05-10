b4698d14c593589bedc44a9b7cbb7f92afe74b56
Daniel Reszka <dannn.r@gmail.com>
2023-05-10 09:50:49 +0200

    Icon: add linked-copy

--- src/routes/Icon.svelte
+++ src/routes/Icon.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  export let kind: 'add' | 'cancel' | 'copy' | 'delete' | 'rename' | 'submit' | 'go'
+  export let kind: 'add' | 'cancel' | 'copy' | 'delete' | 'linked-copy' | 'rename' | 'submit' | 'go'
 </script>
 
 <svg
@@ -32,6 +32,23 @@
       stroke-linecap="round"
       stroke-linejoin="round"
       d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+    />
+  {/if}
+  {#if kind === 'linked-copy'}
+    <path
+      d="M10.2 21.8h4.4c.6 0 1.2-.6 1.2-1.2V9.4c0-.6-.6-1.2-1.2-1.2H8.3v0H4.8c-.6 0-1.2.6-1.2 1.2v3.5"
+      style="fill:none;stroke-linecap:round"
+    />
+    <path
+      d="m11.3 3.8-1 .1c-1.2.1-2 1-2 2.2v2.2m3-4.5c.3-1 1.2-1.5 2.2-1.5H15c1 0 1.9.6 2.1 1.5m-5.8 0v.7c0 .4.3.8.7.8h4.5c.4 0 .8-.4.8-.8 0-.2 0-.4-.2-.7"
+      style="fill:none"
+    />
+    <path d="M15.8 18.8H18c1.2 0 2.3-1 2.3-2.3V6.1c0-1.1-.9-2-2-2.2h-1.1" style="fill:none" />
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M8.4 14.6c1.6.8 2 3 .7 4.2l-2.6 2.6c-2.4 2.4-6-1.2-3.7-3.7l1-1m7.7-.3 1-1c2.4-2.5-1.2-6.1-3.6-3.7l-2.6 2.6a2.6 2.6 0 0 0 .7 4.1"
+      style="stroke-width:1.5;stroke-dasharray:none"
     />
   {/if}
   {#if kind === 'rename'}
