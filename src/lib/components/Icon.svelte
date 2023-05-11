bccd7ed402267495238cddb34e37701d63dd0c0e
Daniel Reszka <dannn.r@gmail.com>
2023-05-12 00:52:25 +0200

    prepare to publish as npm library

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -13,53 +13,27 @@
     | 'go'
 </script>
 
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke-width="1.5"
-  stroke="currentColor"
-  class="w-5 h-5"
->
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
   {#if kind === 'add'}
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
+    <path d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
   {/if}
   {#if kind === 'cancel'}
-    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+    <path d="M6 18L18 6M6 6l12 12" />
   {/if}
   {#if kind === 'copy'}
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
       d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
     />
   {/if}
   {#if kind === 'delete'}
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
       d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
     />
   {/if}
   {#if kind === 'linked-copy-break'}
+    <path d="M13.1 8.8a4.3 4.3 0 0 1 1.2 7L10 20c-4 4-10-2-6-6l1.6-1.7" style="stroke-width:1.5" />
+    <path d="M9.2.9v4.7ZM3.1 3l3.3 3.3ZM.9 9.3h4.8Zm17.4 5.4H23zm-.7 2.8 3.3 3.4zm-2.8.8v4.8z" />
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M13.1 8.8a4.3 4.3 0 0 1 1.2 7L10 20c-4 4-10-2-6-6l1.6-1.7"
-      style="stroke-width:1.5"
-    />
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M9.2.9v4.7ZM3.1 3l3.3 3.3ZM.9 9.3h4.8Zm17.4 5.4H23zm-.7 2.8 3.3 3.4zm-2.8.8v4.8z"
-    />
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
       d="M18.4 11.7 20 10c4-4-2-10-6-6L9.7 8.3a4.3 4.3 0 0 0 1.2 6.9"
       style="stroke-width:1.5"
     />
@@ -75,27 +49,33 @@
     />
     <path d="M15.8 18.8H18c1.2 0 2.3-1 2.3-2.3V6.1c0-1.1-.9-2-2-2.2h-1.1" style="fill:none" />
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
       d="M8.4 14.6c1.6.8 2 3 .7 4.2l-2.6 2.6c-2.4 2.4-6-1.2-3.7-3.7l1-1m7.7-.3 1-1c2.4-2.5-1.2-6.1-3.6-3.7l-2.6 2.6a2.6 2.6 0 0 0 .7 4.1"
       style="stroke-width:1.5;stroke-dasharray:none"
     />
   {/if}
   {#if kind === 'rename'}
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
       d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
     />
   {/if}
   {#if kind === 'submit'}
-    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+    <path d="M4.5 12.75l6 6 9-13.5" />
   {/if}
   {#if kind === 'go'}
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
       d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
     />
   {/if}
 </svg>
+
+<style>
+  svg {
+    stroke: currentColor;
+    fill: none;
+    stroke-width: 1.5;
+  }
+  svg path {
+    stroke-linecap: round;
+    stroke-linejoin: round;
+  }
+</style>
---
+++ src/lib/index.ts
@@ -0,0 +1,4 @@
+export { default as Tree } from './components/Tree.svelte'
+export { default as Branch } from './components/Branch.svelte'
+export { default as Icon } from './components/Icon.svelte'
+export { makeTreeStore } from './components/stores.js'
