742a4ca284b32a85e3ce7d92164855988f1e741e
Daniel Reszka <dannn.r@gmail.com>
2023-05-14 12:13:14 +0200

    stores: add und/redo tests (WIP)

--- src/lib/components/stores.test.ts
+++ src/lib/components/stores.test.ts
@@ -38,3 +38,49 @@ describe('commands', () => {
     })
   })
 })
+
+describe('undo/redo', () => {
+  const commandsToTest2 = {
+    delete: (dagStore: DagStore) => {
+      const node1Id = get(dagStore).children.find((node) => node.name === 'node1')?.id
+      if (!node1Id) throw new Error('node1Id or node2Id not found')
+      dagStore.commands.delete({ id: node1Id })
+    },
+    insert: (dagStore: DagStore) => dagStore.commands.insert({ name: 'node3', parentId: 'ROOT' }),
+    move: (dagStore: DagStore) => {
+      const node1Id = get(dagStore).children.find((node) => node.name === 'node1')?.id
+      const node2Id = get(dagStore).children.find((node) => node.name === 'node2')?.id
+      if (!node1Id || !node2Id) throw new Error('node1Id or node2Id not found')
+      dagStore.commands.move({ id: node2Id, from: 'ROOT', to: node1Id })
+    },
+    rename: (dagStore: DagStore) => {
+      const node2Id = get(dagStore).children.find((node) => node.name === 'node2')?.id
+      if (!node2Id) throw new Error('node2Id not found')
+      dagStore.commands.rename({ id: node2Id, from: 'node2', to: 'node2-renamed' })
+    },
+  }
+
+  Object.entries(commandsToTest2).forEach(([name, commandToTest]) => {
+    it(`works for commands.${name}()`, () => {
+      const dagStore = makeDagStore(rootId, nodes, relations)
+      const { commands } = dagStore
+      commands.insert({ name: 'node1', parentId: 'ROOT' })
+      commands.insert({ name: 'node2', parentId: 'ROOT' })
+
+      const beforeCommand = structuredClone(get(dagStore))
+
+      commandToTest(dagStore)
+      const afterCommand = structuredClone(get(dagStore))
+
+      expect(beforeCommand).not.toEqual(afterCommand)
+
+      commands.undo()
+      const afterUndo = structuredClone(get(dagStore))
+      expect(afterUndo).toEqual(beforeCommand)
+
+      commands.redo()
+      const afterRedo = structuredClone(get(dagStore))
+      expect(afterRedo).toEqual(afterCommand)
+    })
+  })
+})
